26cbc44a4beada21cf2863ba36c107f866b66930
Mr. V <vishnuvardhanreddy31325@gmail.com>
2025-11-03 11:48:35 +0530

    Fix: updated requirements

---
+++ models/__init__.py
@@ -0,0 +1,22 @@
+"""
+Financial Models Package
+Imports all 7 model classes for easy access
+"""
+
+from .model_1_spending_prediction import SpendingPredictionModel
+from .model_2_category_forecast import CategoryForecastModel
+from .model_3_anomaly_detection import AnomalyDetectionModel
+from .model_4_user_segmentation import UserSegmentationModel
+from .model_5_risk_assessment import RiskAssessmentModel
+from .model_6_goal_achievement import GoalAchievementModel
+from .model_7_churn_prediction import ChurnPredictionModel
+
+__all__ = [
+    "SpendingPredictionModel",
+    "CategoryForecastModel",
+    "AnomalyDetectionModel",
+    "UserSegmentationModel",
+    "RiskAssessmentModel",
+    "GoalAchievementModel",
+    "ChurnPredictionModel",
+]
